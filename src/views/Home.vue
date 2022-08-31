2d3c8415a37ccb09e23213ddd6a7a97e5136cb53
Egawa Akiya <egawanomacbook2@EgawanoMacBook-Pro.local>
2022-08-31 17:46:49 +0900

    #219 非同期

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,24 @@
     <div>
         <h3>Home</h3>
         <button @click="toUsers">Usersのページにいく</button>
+        <p>DoubleCount : {{ doubleCount }}</p>
+        <p>TripleCount : {{ tripleCount }}</p>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
+    computed: {
+        ...mapGetters(["doubleCount", "tripleCount"])
+    },
     methods: {
         toUsers() {
-            this.$router.push({ path: "users"});
+            this.$router.push({
+                    name: "users-id-profile",
+                    params: { id: 1 }
+                });
         }
     },
 }
