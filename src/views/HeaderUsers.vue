4a484fa41782ba25caacf21ff30ba60a48bb3092
Egawa Akiya <egawanomacbook2@EgawanoMacBook-Pro.local>
2022-08-31 17:53:13 +0900

    a

---
+++ src/views/HeaderUsers.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav>
+        <h2>Users</h2>
+        <router-link
+            to="/"
+            active-class="link--active"
+            exact
+            class="link"
+            style="padding-right: 15px;"
+        >Home</router-link>
+        <router-link
+            to="/users"
+            active-class="link--active"
+            exact
+            class="link"
+        >Users</router-link>
+    </nav>
+</template>
